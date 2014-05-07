bcaa72be082f2cbbbbb1a6a7d3da87957fed48da
KoryKozlowski <kdkzlwsk@memphis.edu>
2014-05-07 00:37:50 -0500

    Basic Routes/Controllers
    Need to add more routes and flesh out the controllers

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -11,7 +11,50 @@
 |
 */
 
+// index.html
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('index');
+});
+
+// contact.html
+Route::get('contact', function()
+{
+    return View::make('contact');
+});
+
+// register.html
+Route::get('register', function()
+{
+    return View::make('register');
+});
+
+// sign-in.html
+Route::get('login', function()
+{
+    return View::make('login');
+});
+
+// counselor.html
+Route::get('counselor', function()
+{
+    return View::make('counselor');
+});
+
+// student.html
+Route::get('student', function()
+{
+    return View::make('student');
+});
+
+// uni.html
+Route::get('university', function()
+{
+    return View::make('university');
+});
+
+// terms.html
+Route::get('terms', function()
+{
+    return View::make('terms');
 });
