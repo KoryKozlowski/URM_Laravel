e9839860e6def951c73e123f2915f867fe1d45ea
Andrew Hood <andrewhood125@gmail.com>
2014-05-07 23:36:01 -0500

    Clean up with blade templates and fixing html errors.

---
+++ laravel/app/views/layouts/static.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+  <link rel="stylesheet" href="/fonts/font-awesome/css/font-awesome.min.css">
+  <link rel="icon" type="image/png" href="/img/favicon/favicon.ico">
+  <title>University Recruit Me</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="/css/jumbotron.css" rel="stylesheet">
+  <style type="text/css">
+    body {
+      padding-top: 100px;
+    }
+    .navbar-inner{
+        position:relative;
+        padding-left:70px;
+    }
+    .navbar .brand {
+        margin-left: 0px;
+        font-size: 20px;
+        font-weight: 200;
+        color: #777777;
+        text-shadow: 0 1px 0 #ffffff;
+        position: absolute;
+        width: 300px;
+        left: 0px;
+        top: 0px;
+        padding: 10px;
+        z-index: -1;
+    }
+
+    @media (max-width: 1020px) {
+      .navbar-header {
+          float: none;
+      }
+      .navbar-left,.navbar-right {
+          float: none !important;
+      }
+      .navbar-toggle {
+          display: block;
+      }
+      .navbar-collapse {
+          border-top: 1px solid transparent;
+          box-shadow: inset 0 1px 0 rgba(255,255,255,0.1);
+      }
+      .navbar-fixed-top {
+        top: 0;
+        border-width: 0 0 1px;
+      }
+      .navbar-collapse.collapsing {
+        background-color: #222;
+      }
+      .navbar-collapse.collapse {
+          display: none!important;
+          background-color: #222;
+      }
+      .navbar-nav {
+          float: none!important;
+          margin-top: 7.5px;
+      }
+      .navbar-nav>li {
+          float: none;
+      }
+      .navbar-nav>li>a {
+          padding-top: 10px;
+          padding-bottom: 10px;
+      }
+      .collapse.in{
+        display:block !important;
+    }
+  }
+  </style>
+
+  @yield('head')
+</head>
+
+<body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="navbar-inner">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="brand" href="/index"><img src="img/logo_2.png"><font color="#fcb040"> University Recruit Me</font></a>
+      <div class="navbar-collapse collapse">
+
+        <div style="padding-left:10px;padding-right:10px;" class="nav navbar-right">
+      <a href="/login" class="btn btn-primary navbar-btn" id="menu-but">Login</a>
+      <a href="/register" class="btn btn-success navbar-btn" id="menu-but2">Sign up</a>
+    </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a id="nav-about" href="/about">About</a></li>
+          <li><a id="nav-student" href="/student">Student</a></li>
+          <li><a id="nav-counselor" href="/counselor">High School Counselor</a></li>
+          <li><a id="nav-university" href="/university">Universities and Colleges</a></li>
+          <li><a id="nav-contact" href="/contact">Contact</a></li>
+        </ul>
+        
+      </div><!--/.navbar-collapse -->
+      
+      </div>
+      
+  </div>
+
+  @yield('content')
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/tooltips.js"></script>
+  
+  @yield('script')
+  
+</body>
+</html>
---
+++ laravel/app/views/static/about.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.static')
+
+
+@section('head')
+
+<style type="text/css">
+
+img.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+</style>
+
+@stop
+
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <div class="col-xs-6">
+      <p class="lead">UniversityRecruitMe is your innovative, cutting-edge online resource 
+        for students during their high school journey.UniversityRecruitMe is the forerunner
+         of online forums for the next generation of college applicants. URM allows high school 
+         students the ability to promote themselves by communicating their experiences and 
+         achievements with their favorite Universities. This will also allow college admissions 
+         counselors access to a more complete applicant profile. </p>
+    </div>
+
+    <div class="container col-xs-6">
+      <img class="center" src="img/URM_Logo.png">
+      <img class="center" src="img/u3849-4.png">
+    </div>
+  </div>
+
+  <hr>
+
+  <footer>
+    <p>&copy; Company 2014</p>
+  </footer>
+</div> <!-- /container -->
+
+@stop
+
+@section('script')
+  <script type="text/javascript">
+    $("#nav-about").addClass("active");
+  </script>
+  <script>
+    $(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+  </script>
+  <script type="text/javascript">
+    $(function(){
+      $("imageCarousel").carousel();
+    });
+  </script>
+@stop
